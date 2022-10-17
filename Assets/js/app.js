5f075e751b42e3d0bf0584eb4a61081e6f28fecc
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-17 22:30:45 +0530

    feature products are done

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,15 +1,15 @@
 let topProducts = document.getElementById("topProducts");
-function showTopShoes() {
-fetch('https://raw.githubusercontent.com/imhardikdesai/Kiks-Square-Store/master/Assets/js/json/topProducts.json')
-	.then(response => response.json())
-	.then(data => {
-		let shoes = data.shoes;
-		console.log(shoes);
-		let str = "";
-		for (key in shoes) {
-			str += `
+function showTopShoes(shoesNumbers) {
+	fetch('https://raw.githubusercontent.com/imhardikdesai/Kiks-Square-Store/master/Assets/js/json/topProducts.json')
+		.then(response => response.json())
+		.then(data => {
+			let shoes = data.shoes;
+			console.log(shoes);
+			let str = "";
+			for (let i = 0; i < shoesNumbers; i++) {
+				str += `
 			<div class="p-body">
-                <img class="shoesImg" src="${shoes[key].imgSrc}" alt="shoes">
+                <img class="shoesImg" src="${shoes[i].imgSrc}" alt="shoes">
                 <div class="rating">
                     <i class="fa fa-star clicked"></i>
                     <i class="fa fa-star clicked"></i>
@@ -18,14 +18,14 @@ fetch('https://raw.githubusercontent.com/imhardikdesai/Kiks-Square-Store/master/
                     <i class="fa fa-star"></i>
                 </div>
 				<div class="top-p-text">
-                <p class="top-p-name">${shoes[key].name}</p>
-                <p class="top-p-price">${shoes[key].price}</p>
+                <p class="top-p-name">${shoes[i].name}</p>
+                <p class="top-p-price">${shoes[i].price}</p>
 				</div>
             </div>
 			`;
-		}
-		topProducts.innerHTML = str;
-	})
-	.catch(err => console.error(err));
+			}
+			topProducts.innerHTML = str;
+		})
+		.catch(err => console.error(err));
 }
-showTopShoes()
+showTopShoes('4');
