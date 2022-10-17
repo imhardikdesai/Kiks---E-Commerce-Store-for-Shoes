84a46e5d88d5dc762e245dca19bfbc5c24257cfd
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-17 21:27:10 +0530

    add more items json

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,4 +1,28 @@
-fetch('json/topProducts.json')
+let topProducts = document.getElementById("topProducts");
+
+fetch('https://raw.githubusercontent.com/imhardikdesai/Kiks-Square-Store/master/Assets/js/json/topProducts.json')
 	.then(response => response.json())
-	.then(data => console.log(data))
+	.then(data => {
+		let shoes = data.shoes;
+		let str = "";
+		for (key in shoes) {
+			str += `
+			<div class="p-body">
+                <img class="shoesImg" src="${shoes[key].imgSrc}" alt="shoes">
+                <div class="rating">
+                    <i class="fa fa-star clicked"></i>
+                    <i class="fa fa-star clicked"></i>
+                    <i class="fa fa-star clicked"></i>
+                    <i class="fa fa-star clicked"></i>
+                    <i class="fa fa-star"></i>
+                </div>
+				<div class="top-p-text">
+                <p class="top-p-name">${shoes[key].name}</p>
+                <p class="top-p-price">${shoes[key].price}</p>
+				</div>
+            </div>
+			`;
+		}
+		topProducts.innerHTML = str;
+	})
 	.catch(err => console.error(err));
