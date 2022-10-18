0f40858d118ba75aed12e303dc39641f4a24f34e
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-18 17:48:17 +0530

    update json

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -1,17 +1,16 @@
 let topProducts = document.getElementById("topProducts");
-function showTopShoes(shoesNumbers) {
+function showTopShoes() {
 	fetch('https://raw.githubusercontent.com/imhardikdesai/Kiks-Square-Store/master/Assets/js/json/topProducts.json')
 		.then(response => response.json())
 		.then(data => {
 			let shoes = data.shoes;
 			console.log(shoes);
 			let str = "";
-			for (let i = 0; i < shoesNumbers; i++) {
-				console.log(shoes[i].id);
+			for (key in shoes) {
 				str += `
 			<div class="p-body">
-			<div class="circle" style="${shoes[i].bgGradient}"></div>
-                <img class="shoesImg" src="${shoes[i].imgSrc}" alt="shoes">
+			<div class="circle" style="${shoes[key].bgGradient}"></div>
+                <img class="shoesImg" src="${shoes[key].imgSrc}" alt="shoes">
                 <div class="rating">
                     <i class="fa fa-star clicked"></i>
                     <i class="fa fa-star clicked"></i>
@@ -20,8 +19,8 @@ function showTopShoes(shoesNumbers) {
                     <i class="fa fa-star"></i>
                 </div>
 				<div class="top-p-text">
-                	<p class="top-p-name">${shoes[i].name}</p>
-					<p class="top-p-price">${shoes[i].price}<span id="${shoes[i].id}" onClick="addProduct(this.id)" class="material-symbols-outlined addIcon">add_shopping_cart</span></p>
+                	<p class="top-p-name">${shoes[key].name}</p>
+					<p class="top-p-price">${shoes[key].price}<span id="${shoes[key].id}" onClick="addProduct(this.id)" class="material-symbols-outlined addIcon">add_shopping_cart</span></p>
 				</div>
             </div>
 			`;
@@ -30,9 +29,13 @@ function showTopShoes(shoesNumbers) {
 		})
 		.catch(err => console.error(err));
 }
-showTopShoes('4');
+showTopShoes();
 
 
-function addProduct(id){
+let cartCount = document.getElementById("cartCount");
+let count = 0;
+function addProduct(id) {
+	count++;
+	cartCount.innerHTML = count;
 	console.log(id);
 }
