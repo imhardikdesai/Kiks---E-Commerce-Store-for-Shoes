6192ce0916d82016697a783154ce8c2deb018e64
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-18 22:22:38 +0530

    responsive top seller products

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -6,11 +6,11 @@ function showTopShoes() {
 			let shoes = data.shoes;
 			console.log(shoes);
 			let str = "";
-			for (key in shoes) {
+			for (let i = 0; i < 4; i++) {
 				str += `
 			<div class="p-body">
-			<div class="circle" style="${shoes[key].bgGradient}"></div>
-                <img class="shoesImg" src="${shoes[key].imgSrc}" alt="shoes">
+			<div class="circle" style="${shoes[i].bgGradient}"></div>
+                <img class="shoesImg" src="${shoes[i].imgSrc}" alt="shoes">
                 <div class="rating">
                     <i class="fa fa-star clicked"></i>
                     <i class="fa fa-star clicked"></i>
@@ -19,8 +19,8 @@ function showTopShoes() {
                     <i class="fa fa-star"></i>
                 </div>
 				<div class="top-p-text">
-                	<p class="top-p-name">${shoes[key].name}</p>
-					<p class="top-p-price">${shoes[key].price}<span id="${shoes[key].id}" onClick="addProduct(this.id)" class="material-symbols-outlined addIcon">add_shopping_cart</span></p>
+                	<p class="top-p-name">${shoes[i].name}</p>
+					<p class="top-p-price">${shoes[i].price}<span id="${shoes[i].id}" onClick="addProduct(this.id)" class="material-symbols-outlined addIcon">add_shopping_cart</span></p>
 				</div>
             </div>
 			`;
@@ -40,5 +40,5 @@ function addProduct(id) {
 	console.log(id);
 }
 
-// women shoes 
+// women shoes
 // https://user-images.githubusercontent.com/87645745/196430139-c49f6ea2-caab-4626-bae1-78fd52b09f9c.png
