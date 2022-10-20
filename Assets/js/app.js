5a3887ddd33286a7988b77842e29686d87473614
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-20 13:44:59 +0530

    created footer

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -46,8 +46,10 @@ function displayNewShoes() {
 	if (toggleGender.checked == true) {
 		sellerShoes.setAttribute('src', './Assets/img/best-seller-women.png');
 		document.getElementById('shoesDisplay').style.background = 'linear-gradient(270deg, #FF3C78 0%, #FFB2B2 100%) ';
+		document.getElementById('bestSellerShoesText').innerText = 'Adidas Falcon Shoes for women - 2021 Edition (PINK)';
 	} else {
 		sellerShoes.setAttribute('src', './Assets/img/best-seller-men.png');
 		document.getElementById('shoesDisplay').style.background = 'linear-gradient(270deg, #2226d4 0%, #98cef9 100%) ';
+		document.getElementById('bestSellerShoesText').innerText = 'Adidas Falcon Shoes for men - 2021 Edition (BLUE)';
 	}
 }
