7819591cfd14631c48153835769b116a1ba73c85
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2022-10-22 14:01:45 +0530

    Update json with more topProducts

--- Assets/js/app.js
+++ Assets/js/app.js
@@ -23,18 +23,18 @@ function topFunction() {
 
 
 let topProducts = document.getElementById("topProducts");
-function showTopShoes() {
+function showTopShoes(startIndex, endIndex) {
 	fetch('https://raw.githubusercontent.com/imhardikdesai/Kiks-Square-Store/master/Assets/js/json/topProducts.json')
 		.then(response => response.json())
 		.then(data => {
 			let shoes = data.shoes;
 			console.log(shoes);
 			let str = "";
-			for (let i = 0; i < 4; i++) {
+			for (startIndex; startIndex < endIndex; startIndex++) {
 				str += `
 			<div class="p-body">
-			<div class="circle" style="background: linear-gradient(239deg, ${shoes[i].bgGradient[0]} 0%, ${shoes[i].bgGradient[1]} 100%);"></div>
-                <img class="shoesImg" src="${shoes[i].imgSrc}" alt="shoes">
+			<div class="circle" style="background: linear-gradient(239deg, ${shoes[startIndex].bgGradient[0]} 0%, ${shoes[startIndex].bgGradient[1]} 100%);"></div>
+                <img class="shoesImg" src="${shoes[startIndex].imgSrc}" alt="shoes">
                 <div class="rating">
                     <i class="fa fa-star clicked"></i>
                     <i class="fa fa-star clicked"></i>
@@ -43,8 +43,8 @@ function showTopShoes() {
                     <i class="fa fa-star"></i>
                 </div>
 				<div class="top-p-text">
-                	<p class="top-p-name">${shoes[i].name}</p>
-					<p class="top-p-price">${shoes[i].price}<span id="${shoes[i].id}" onClick="addProduct(this.id)" class="material-symbols-outlined addIcon">add_shopping_cart</span></p>
+                	<p class="top-p-name">${shoes[startIndex].name}</p>
+					<p class="top-p-price">${shoes[startIndex].price}<span id="${shoes[startIndex].id}" onClick="addProduct(this.id)" class="material-symbols-outlined addIcon">add_shopping_cart</span></p>
 				</div>
             </div>
 			`;
@@ -53,7 +53,7 @@ function showTopShoes() {
 		})
 		.catch(err => console.error(err));
 }
-showTopShoes();
+showTopShoes(0, 4);
 
 
 let cartCount = document.getElementById("cartCount");
